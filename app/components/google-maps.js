e629165a3413687d5d71bc0a6e06fd2612a3119f
Rodney Folz <rodney@rodneyfolz.com>
2014-10-26 07:15:06 -0400

    Snazzy maps

--- app/components/google-maps.js
+++ app/components/google-maps.js
@@ -2,6 +2,8 @@
 
 import Ember from 'ember';
 
+var mapStyle = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
+
 export default Ember.Component.extend({
     insertMap: function() {
         var container = this.$(".map-canvas");
@@ -10,7 +12,8 @@ export default Ember.Component.extend({
             center: new google.maps.LatLng(this.get('latitude'), this.get('longitude')),
             zoom: parseInt(this.get('zoom')),
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            streetViewControl: false
+            streetViewControl: false,
+            styles: mapStyle
         };
 
         this.set('map', new google.maps.Map(container[0], options));
